921e3e9919a669f7255542d597ecc61362aad364
Yefta Asyel <yeftakun34@gmail.com>
2023-12-03 21:14:36 +0800

    ONGOING-1

--- GUI.py
+++ GUI.py
@@ -1,6 +1,6 @@
 # gui.py
 from flask import Flask, render_template, request
-from app import user_login, show_biaya_denda, show_nama
+from app import user_login, show_nama, total_books, total_users, dipinjam, dikembalikan, levellogin, meminjam #, show_biaya_denda
 
 app = Flask(__name__)
 
@@ -13,14 +13,40 @@ def login():
     username = request.form['username']
     password = request.form['password']
     user_data = user_login(username, password)
-
-    if user_data:
-        nama_result = show_nama(username)  # Menggunakan username sebagai parameter
-        return render_template('dashboard.html', myname=nama_result[0][0], data=show_biaya_denda())
-    else:
-        return render_template('index.html', error="Login failed. Incorrect username or password.")
+    # user_level = levellogin(username)
+    level_result = levellogin(username)
+    h1 = (level_result[0][0])=='Petugas'
+    h2 = (level_result[0][0])=='Anggota'
+    user_level1 = h1
+    user_level2 = h2
     
 
+    if user_data and user_level1:
+        nama_result = show_nama(username)  # Menggunakan username sebagai parameter
+        total_buku = total_books()
+        total_user = total_users()
+        total_dipinjam = dipinjam()
+        total_dikembalikan = dikembalikan()
+        return render_template('petugas/dashboard.html', 
+                               myname=nama_result[0][0], 
+                               mylevel=level_result[0][0],
+                               countbook=total_buku[0][0], 
+                               countuser=total_user[0][0], 
+                               countpinjam=total_dipinjam[0][0],
+                               countkembali=total_dikembalikan[0][0]
+                               )#, data=show_biaya_denda()
+    elif user_data and user_level2:     # yang ini buat anggota
+        nama_result = show_nama(username)  # Menggunakan username sebagai parameter
+        total_buku = total_books()
+        total_dipinjam = meminjam(username)
+        return render_template('anggota/dashboard.html', 
+                               myname=nama_result[0][0], 
+                               mylevel=level_result[0][0],
+                               countbook=total_buku[0][0], 
+                               countpinjam=total_dipinjam[0][0]
+                               )
+    else:
+        return render_template('index.html', error="Login failed. Incorrect username or password.")
 
 if __name__ == '__main__':
     app.run(debug=True)
--- app.py
+++ app.py
@@ -21,19 +21,75 @@ def user_login(username, password):
     return result
 
 # Fungsi untuk menampilkan data biaya denda
-def show_biaya_denda():
-    connection = create_connection()
-    cursor = connection.cursor()
-    cursor.execute("SELECT * FROM tbl_biaya_denda")
-    result = cursor.fetchall()
-    connection.close()
-    return result
+# def show_biaya_denda():
+#     connection = create_connection()
+#     cursor = connection.cursor()
+#     cursor.execute("SELECT * FROM tbl_biaya_denda")
+#     result = cursor.fetchall()
+#     connection.close()
+#     return result
 
+# menampilkan nama pengguna yang login
 def show_nama(username):
     connection = create_connection()
     cursor = connection.cursor()
     query = "SELECT nama FROM tbl_login WHERE user = %s"
     cursor.execute(query, (username,))
+    result = cursor.fetchall()
+    connection.close()
+    return result
+
+
+def levellogin(username):
+    connection = create_connection()
+    cursor = connection.cursor()
+    query = "SELECT level FROM tbl_login WHERE user = %s"
+    cursor.execute(query, (username,))
+    result = cursor.fetchall()
+    connection.close()
+    return result
+
+def total_books():
+    connection = create_connection()
+    cursor = connection.cursor()
+    query = "SELECT count(title) FROM tbl_buku"
+    cursor.execute(query)
+    result = cursor.fetchall()
+    connection.close()
+    return result
+
+def total_users():
+    connection = create_connection()
+    cursor = connection.cursor()
+    query = "SELECT count(nama) FROM tbl_login where level = 'anggota'"
+    cursor.execute(query)
+    result = cursor.fetchall()
+    connection.close()
+    return result
+
+def dipinjam():
+    connection = create_connection()
+    cursor = connection.cursor()
+    query = "select count(status) from tbl_pinjam where status = 'dipinjam'"
+    cursor.execute(query)
+    result = cursor.fetchall()
+    connection.close()
+    return result
+
+def dikembalikan():
+    connection = create_connection()
+    cursor = connection.cursor()
+    query = "select count(status) from tbl_pinjam where status = 'di kembalikan'"
+    cursor.execute(query)
+    result = cursor.fetchall()
+    connection.close()
+    return result
+
+def meminjam(username):
+    connection = create_connection()
+    cursor = connection.cursor()
+    query = "select count(status) from tbl_pinjam where status='dipinjam' and anggota_id=(select anggota_id from tbl_login where user= %s)"
+    cursor.execute(query,(username,))
     result = cursor.fetchall()
     connection.close()
     return result
